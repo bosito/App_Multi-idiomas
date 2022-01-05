7a3e19576ef8927cea9f6c3bc0d1ee9e9d0d20d6
Luis Mac <luis@dwitmexico.com>
2022-01-05 17:50:38 -0500

    test de app multilenguage

--- App.js
+++ App.js
@@ -1,21 +1,73 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import './assets/¡18n/¡18n';
+import { StyleSheet, Text, View, Button, Alert } from 'react-native';
+import { useTranslation } from 'react-i18next';
 
 export default function App() {
+
+  const { t, i18n } = useTranslation();
+
+  const [language, setLanguage] = useState('es');
+  const [languageIs, setLanguageIs] = useState("");
+  const [testConditional, setTestConditional] = useState(false);
+
+  useEffect(() => {
+    const stateLanguage = setStateLanguage();
+    setLanguageIs(stateLanguage);
+  }, [language])
+
+
+  const testChangeLanguage = value => {
+    i18n
+      .changeLanguage(value)
+      .then(() => setLanguage(value))
+      .catch(err => console.log(err));
+  };
+
+  const setStateLanguage = () => {
+    switch (language) {
+      case 'es':
+        return "Español"
+      case 'hi':
+        return 'Hindi'
+      default:
+        return 'English'
+    }
+  }
+
+  const testAtert = () => Alert.alert("",t('texto de ejemplo'))
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ ...styles.container, justifyContent: 'center' }}>
+      <View style={{ marginTop: 50 }}>
+        <Text style={{ fontWeight: 'bold' }} >{languageIs}</Text>
+        <Text>{t('texto de ejemplo')}</Text>
+        <Text>{t('este es un nuevo texto')}</Text>
+        <Text>
+          {
+            testConditional ? t('texto de ejemplo') : t('este es un nuevo texto')
+          }
+        </Text>
+      </View>
+
+
+      <View style={styles.container}>
+        <Button title="ingles" onPress={() => testChangeLanguage('en')} />
+        <Button title="español" onPress={() => testChangeLanguage('es')} />
+        <Button title="hindu" onPress={() => testChangeLanguage('hi')} />
+        <Button title="cambio estado" onPress={() => setTestConditional(!testConditional)} />
+        <Button title={languageIs} onPress={testAtert} />
+      </View>
+
     </View>
   );
-}
+};
+
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
-  },
+    justifyContent: 'space-around'
+  }
 });
